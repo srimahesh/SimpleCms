f1daef4fad8096e1966dec6685758be9b6e820c0
Mahesh Vutukuri <vutukurim@gmail.com>
2014-02-10 17:22:56 -0600

    Inital Commit

---
+++ app/controllers/demo_controller.rb
@@ -0,0 +1,7 @@
+class DemoController < ApplicationController
+  
+  # layout false
+  # Action when in context with a controller, uses a layout too
+  def index
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SimpleCms::Application.config.secret_key_base = 'cf9b4bbe9ba4ea85241085429dc342855745f05e636eae7c2be27f3c5215a6b9ec3e0baeba363de4b8b356f62717a9ce7a3a0f1cf7a60266759813ef409cf547'
